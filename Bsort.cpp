b87517fce40e1ad5048d8ba3da921c7536eb5923
dancher <cherepanodan@rambler.ru>
2014-03-08 12:57:01 +0400

    commit

---
+++ Bsort.cpp
@@ -0,0 +1,15 @@
+#define SWAP(A, B) { char t = A; A = B; B = t; }
+ 
+void bubblesort(char *a, int n)
+{
+  int i, j;
+ 
+  for (i = n - 1; i > 0; i--)
+  {
+    for (j = 0; j < i; j++)
+    {
+      if (a[j] > a[j + 1]) 
+        SWAP( a[j], a[j + 1] );
+    }
+  }
+}
---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include "iostream"
+#include "Bsort.cpp"
+#include "Msort.cpp"
+#pragma once
+using namespace std;
+
+void main()
+{
+	char mass1 [10];
+	char mass2 [10];
+	for (int i=0;i<10;i++)
+	{
+		cin>>mass1[i]; 
+	}
+	
+	bubblesort(mass1,10);
+	
+	for (int i=0;i<10;i++)
+	{
+		cout<<mass1[i]; 
+	}
+	
+	for (int i=0;i<10;i++)
+	{
+		cin>>mass2[i]; 
+	}
+	
+	merge_sort(mass2,0,9) ;
+
+	for (int i=0;i<10;i++)
+	{
+		cout<<mass2[i]; 
+	}
+		  
+}
